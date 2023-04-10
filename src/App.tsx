5d7e53b1c1bd84c930aa84f761a8b98e3c1e1244
Rohan Arora <mac@macs-MacBook-Air.local>
2023-04-10 22:48:24 +0530

    Added explore top books layout in home page folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
 import { Navbar } from './layouts/navbarAndFooter/Navbar';
+import { ExploteTopBooks } from './layouts/HomePage.tsx/ExploreTopBooks';
 
 function App() {
   return (
-    <Navbar/>
+    <div>
+      <Navbar/>
+      <ExploteTopBooks/>
+    </div>
   );
 }
 
---
+++ src/layouts/HomePage.tsx/ExploreTopBooks.tsx
@@ -0,0 +1,16 @@
+export const ExploteTopBooks = () => {
+    return(
+        <div className="p-5 mb-4 bg-dark header">
+            <div className="container-fluid py-5 text-white 
+                d-flex justify-content-center align-items-center">
+                <div>
+                    <h1 className="display-5 fw-bold"> What next?</h1>
+                    <p className="col-md-8 fs-4">Ok, find your next go to place to read!</p>
+                    <a type="button" className="btn main-color btn-lg text-white" href="#">
+                        Explore top books
+                    </a>
+                </div>
+            </div>
+        </div>
+    )
+}
