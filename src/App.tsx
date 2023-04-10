fbb3faa0da6c970d6a8686b54c8fcf6f43870ff7
Rohan Arora <mac@macs-MacBook-Air.local>
2023-04-10 22:37:10 +0530

    Added home page layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
+import { Navbar } from './layouts/navbarAndFooter/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome babyy!</h1>
-    </div>
+    <Navbar/>
   );
 }
 
