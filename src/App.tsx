2bf2a7e22b42f79dfe3c6e69d165e91a25eaa062
Rohan Arora <mac@macs-MacBook-Air.local>
2023-04-11 22:47:34 +0530

    Added heroes component

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import './App.css';
 import { Navbar } from './layouts/navbarAndFooter/Navbar';
 import { ExploteTopBooks } from './layouts/HomePage/ExploreTopBooks';
 import Carousal from './layouts/HomePage/Carousal'; 
+import { Heros } from './layouts/HomePage/Heros';
 function App() {
   return (
     <div>
       <Navbar/>
       <ExploteTopBooks/>
       <Carousal/>
+      <Heros />
     </div>
   );
 }
---
+++ src/layouts/HomePage/LibraryService.tsx
@@ -0,0 +1,5 @@
+export const LibraryService = () => {
+    return(
+        <div></div>
+    ); 
+}
