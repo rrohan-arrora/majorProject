e2edc15c5e7163e06845fa0e62ec22964ffc7a7b
Rohan Arora <mac@macs-MacBook-Air.local>
2023-04-17 15:25:17 +0530

    Added book model

---
+++ src/models/BookModel.ts
@@ -0,0 +1,28 @@
+export class BookModel{
+    id: number;
+    title: string;
+    author?: string;
+    description?: string;
+    copies?: number;
+    copies_available?: number;
+    category?: string;
+    img?: string;
+
+    constructor(id: number, 
+                title: string, 
+                author: string,
+                description: string,
+                copies: number,
+                copies_available: number,
+                category: string,
+                img: string){
+                    this.id = id;
+                    this.title = title;
+                    this.author = author;
+                    this.description = description;
+                    this.copies = copies;
+                    this.copies_available = copies_available;
+                    this.category = category;
+                    this.img = img;
+                }
+}
