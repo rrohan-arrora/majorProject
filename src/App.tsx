98cd484e590b71d42e010cf3ea4d60107af63ac3
Rohan Arora <mac@macs-MacBook-Air.local>
2023-04-18 23:33:10 +0530

    Added react router Implemented Switch functionality in react-router Installed react-router-dom@5

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,28 @@ import { Navbar } from './layouts/navbarAndFooter/Navbar';
 import { Footer } from './layouts/navbarAndFooter/Footer';
 import { HomePage } from './layouts/HomePage/HomePage';
 import { SearchBookPage } from './layouts/SearchBookPage/SearchBookPage';
+import { Redirect, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <Navbar/>
-      {/* <HomePage /> */}
-      <SearchBookPage />
+      <Navbar />
+      {/* Switch helps in  handling the rendering 
+      of single route. Before , both homepage and 
+      search page was rendering. Also, always home page 
+      was rendering as "/"" matches even of we do "/search".
+      In order to handle that, we used exactl*/}
+      <Switch> 
+        <Route path="/" exact>
+          <Redirect to='/home' />
+        </Route>
+        <Route path="/home">
+          <HomePage />
+        </Route>
+        <Route path='/search'>
+          <SearchBookPage />
+        </Route>
+      </Switch>
       <Footer />
     </div>
   );
