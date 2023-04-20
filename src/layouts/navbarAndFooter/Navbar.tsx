311fbe18ecbf890d7893a71c2de6a4790272ed05
Rohan Arora <mac@macs-MacBook-Air.local>
2023-04-20 16:09:19 +0530

    Added logout functionality

--- src/layouts/navbarAndFooter/Navbar.tsx
+++ src/layouts/navbarAndFooter/Navbar.tsx
@@ -1,6 +1,16 @@
 import { NavLink } from "react-router-dom";
+import { useOktaAuth } from "@okta/okta-react";
 
 export const Navbar = () => {
+
+    const { oktaAuth, authState } = useOktaAuth();
+    if (!authState) {
+        return (
+            <div>Loading.....</div>
+        )
+    }
+    const handleLogout = async () => oktaAuth.signOut();
+    console.log(authState);
     return (
         <nav className='navbar navbar-expand-lg navbar-dark main-color py-3'>
             <div className="container-fluid">
@@ -22,9 +32,16 @@ export const Navbar = () => {
                         </li>
                     </ul>
                     <ul className='navbar-nav ms-auto'>
-                        <li className='nav-item m-1'>
-                            <a type="button" className="btn btn-outline-light" href="#">Sign In</a>
-                        </li>
+                        {!authState.isAuthenticated ?
+                            <li className='nav-item m-1'>
+                                <Link type="button" className="btn btn-outline-light" to="/login">Sign In</Link>
+                            </li>
+                            :
+                            <li className='nav-item m-1'>
+                                <button className="btn btn-outline-light" onClick={handleLogout}>Logout</button>
+                            </li>
+                        }
+
                     </ul>
                 </div>
             </div>
