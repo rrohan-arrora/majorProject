f7ee764a4b0e6f6d494e857f20e3cfd898586f42
Rohan Arora <an14092000@gmail.com>
2023-04-24 22:36:11 +0530

    Added mobile view for the loans page

--- src/layouts/ShelfPage/components/Loans.tsx
+++ src/layouts/ShelfPage/components/Loans.tsx
@@ -13,7 +13,7 @@ export const Loans = () => {
     useEffect(() => {
         const fetchUserCurrentLoans = async () => {
             if (authState?.isAuthenticated) {
-                const url = `http://localhost:1111/api/books/secure/currentloans`;
+                const url = `http://localhost:1111/api/books/secure/currentLoans`;
                 const requestOptions = {
                     method: 'GET',
                     headers: {
@@ -97,7 +97,7 @@ export const Loans = () => {
                                                         data-bs-target={`#modal${shelfCurrentLoan.book.id}`}>
                                                         Manage Loan
                                                     </button>
-                                                    <Link to={'search'} className='list-group-item list-group-item-action'>
+                                                    <Link to="/search" className='list-group-item list-group-item-action'>
                                                         Search more books?
                                                     </Link>
                                                 </div>
@@ -126,6 +126,78 @@ export const Loans = () => {
                     </>
                 }
             </div>
+
+            {/* Mobile */}
+            <div className='container d-lg-none mt-2'>
+                {shelfCurrentLoans.length > 0 ? 
+                <>
+                    <h5 className='mb-3'>Current Loans: </h5>
+
+                    {shelfCurrentLoans.map(shelfCurrentLoan => (
+                        <div key={shelfCurrentLoan.book.id}>
+                                <div className='d-flex justify-content-center align-items-center'>
+                                    {shelfCurrentLoan.book?.img ? 
+                                        <img src={shelfCurrentLoan.book?.img} width='226' height='349' alt='Book'/>
+                                        :
+                                        <img src={require('./../../../Images/BooksImages/book-1000.png')} 
+                                            width='226' height='349' alt='Book'/>
+                                    }
+                                </div>
+                                <div className='card d-flex mt-5 mb-3'>
+                                    <div className='card-body container'>
+                                        <div className='mt-3'>
+                                            <h4>Loan Options</h4>
+                                            {shelfCurrentLoan.daysLeft > 0 && 
+                                                <p className='text-secondary'>
+                                                    Due in {shelfCurrentLoan.daysLeft} days.
+                                                </p>
+                                            }
+                                            {shelfCurrentLoan.daysLeft === 0 && 
+                                                <p className='text-success'>
+                                                    Due Today.
+                                                </p>
+                                            }
+                                            {shelfCurrentLoan.daysLeft < 0 && 
+                                                <p className='text-danger'>
+                                                    Past due by {shelfCurrentLoan.daysLeft} days.
+                                                </p>
+                                            }
+                                            <div className='list-group mt-3'>
+                                                <button className='list-group-item list-group-item-action' 
+                                                    aria-current='true' data-bs-toggle='modal' 
+                                                    data-bs-target={`#mobilemodal${shelfCurrentLoan.book.id}`}>
+                                                        Manage Loan
+                                                </button>
+                                                <Link to={'search'} className='list-group-item list-group-item-action'>
+                                                    Search more books?
+                                                </Link>
+                                            </div>
+                                        </div>
+                                        <hr/>
+                                        <p className='mt-3'>
+                                            Help other find their adventure by reviewing your loan.
+                                        </p>
+                                        <Link className='btn btn-primary' to={`/checkout/${shelfCurrentLoan.book.id}`}>
+                                            Leave a review
+                                        </Link>
+                                    </div>
+                                </div>
+                            
+                            <hr/>
+                        </div>
+                    ))}
+                </> :
+                <>
+                    <h3 className='mt-3'>
+                        Currently no loans
+                    </h3>
+                    <Link className='btn btn-primary' to={`search`}>
+                        Search for a new book
+                    </Link>
+                </>
+            }
+            </div>
+
         </div>
     );
 }
