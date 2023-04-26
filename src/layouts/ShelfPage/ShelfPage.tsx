bf40f126038064803a76f42c84f1a491d1fd7b08
Rohan Arora <an14092000@gmail.com>
2023-04-26 23:31:51 +0530

    Added history model Added history page to show the book checked out by the user called the history page from the shelf page

--- src/layouts/ShelfPage/ShelfPage.tsx
+++ src/layouts/ShelfPage/ShelfPage.tsx
@@ -1,17 +1,21 @@
 import { useState } from "react";
 import { Loans } from "./components/Loans";
+import { HistoryPage } from "./components/HistoryPage";
 export const ShelfPage = () => {
+
+    const [history, setHistory] = useState(false);
+
     return (
         <div className='container'>
             <div className='mt-3'>
                 <nav>
                     <div className='nav nav-tabs' id='nav-tab' role='tablist'>
-                        <button className='nav-link active' id='nav-loans-tab' data-bs-toggle='tab'
+                        <button onClick={() => setHistory(false)} className='nav-link active' id='nav-loans-tab' data-bs-toggle='tab'
                             data-bs-target='#nav-loans' type='button' role='tab' aria-controls='nav-loans'
                             aria-selected='true'>
                                 Loans
                         </button>
-                        <button className='nav-link' id='nav-history-tab' data-bs-toggle='tab' 
+                        <button onClick={() => setHistory(true)} className='nav-link' id='nav-history-tab' data-bs-toggle='tab' 
                             data-bs-target='#nav-history' type='button' role='tab' aria-controls='nav-history'
                             aria-selected='false'>
                                 Your History
@@ -25,7 +29,7 @@ export const ShelfPage = () => {
                     </div>
                     <div className='tab-pane fade' id='nav-history' role='tabpanel'
                         aria-labelledby='nav-history-tab'>
-                            CHECKOUT HISTORY
+                            {history ? <HistoryPage /> : <></>}
                     </div>
                 </div>
             </div>
