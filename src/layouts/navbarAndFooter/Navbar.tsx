a24c964583d5dc762937aa3650ee2366fb39a180
aakash1998gadekar <aakash.gadekar@iiitb.ac.in>
2023-04-28 19:43:57 +0530

    Added secure https routing to the project Read the doc from https://luv2code.com/react-openssl-setup

--- src/layouts/navbarAndFooter/Navbar.tsx
+++ src/layouts/navbarAndFooter/Navbar.tsx
@@ -36,6 +36,11 @@ export const Navbar = () => {
                                 <NavLink className="nav-link" to="/shelf">Shelf</NavLink>
                             </li>
                         }
+                        {authState.isAuthenticated && authState.accessToken?.claims?.userType === 'admin' && 
+                            <li className='nav-item'>
+                                <NavLink className="nav-link" to="/admin">Library Admin Facilities</NavLink>
+                            </li>
+                        }
                     </ul>
                     <ul className='navbar-nav ms-auto'>
                         {!authState.isAuthenticated ?
